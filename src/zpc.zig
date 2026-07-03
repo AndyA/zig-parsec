6186f7a8d86874e43cec2b70a25b429c485b8600
Andy Armstrong <andy@hexten.net>
2026-07-03 11:12:06 +0100

    Snapshot

--- src/zpc.zig
+++ src/zpc.zig
@@ -76,7 +76,7 @@ pub fn ZpcParser(comptime Context: type, comptime Tag: type) type {
     return fn (ctx: *Context, input: []const u8) ZpcError!ZpcResult(Tag);
 }
 
-const TestTag = enum { HELLO, FOO };
+const TestTag = enum { HELLO, FOO, BAR, NEWLINE };
 
 const TestContext = struct {
     allocator: Allocator,
@@ -125,26 +125,60 @@ pub fn Zpc(comptime Context: type, comptime Tag: type) type {
         }
 
         test lit {
-            const hello = TZ.lit(.HELLO, "Hello");
+            const parseHello = lit(.HELLO, "Hello");
 
             var ctx: TestContext = .{ .allocator = std.testing.allocator };
 
             try checkAndConsume(
                 ctx,
                 .initOk(.initSlice(.HELLO, "Hello"), ", World"),
-                try hello(&ctx, "Hello, World"),
+                try parseHello(&ctx, "Hello, World"),
             );
 
             try checkAndConsume(
                 ctx,
                 .initFail("H"),
-                try hello(&ctx, "H"),
+                try parseHello(&ctx, "H"),
             );
 
             try checkAndConsume(
                 ctx,
                 .initFail("Hell or bust"),
-                try hello(&ctx, "Hell or bust"),
+                try parseHello(&ctx, "Hell or bust"),
+            );
+        }
+
+        pub fn oneOf(tag: Tag, chars: []const u8) Parser {
+            const shim = struct {
+                fn oneOfParser(_: *Context, input: []const u8) ZpcError!Result {
+                    if (input.len > 0 and std.mem.containsAtLeastScalar(u8, chars, input[0], 1))
+                        return .initOk(.initSlice(tag, input[0..1]), input[1..]);
+                    return .initFail(input);
+                }
+            };
+            return shim.oneOfParser;
+        }
+
+        test oneOf {
+            const parseNewline = oneOf(.NEWLINE, "\n\r");
+            var ctx: TestContext = .{ .allocator = std.testing.allocator };
+
+            try checkAndConsume(
+                ctx,
+                .initOk(.initSlice(.NEWLINE, "\n"), "\r"),
+                try parseNewline(&ctx, "\n\r"),
+            );
+
+            try checkAndConsume(
+                ctx,
+                .initOk(.initSlice(.NEWLINE, "\r"), "\n"),
+                try parseNewline(&ctx, "\r\n"),
+            );
+
+            try checkAndConsume(
+                ctx,
+                .initFail("X"),
+                try parseNewline(&ctx, "X"),
             );
         }
 
@@ -164,9 +198,9 @@ pub fn Zpc(comptime Context: type, comptime Tag: type) type {
         }
 
         test alt {
-            const oneOf = TZ.alt(&.{
-                TZ.lit(.HELLO, "Hello"),
-                TZ.lit(.FOO, "Foo"),
+            const parseAlt = alt(&.{
+                lit(.HELLO, "Hello"),
+                lit(.FOO, "Foo"),
             });
 
             var ctx: TestContext = .{ .allocator = std.testing.allocator };
@@ -174,19 +208,102 @@ pub fn Zpc(comptime Context: type, comptime Tag: type) type {
             try checkAndConsume(
                 ctx,
                 .initOk(.initSlice(.HELLO, "Hello"), ", World"),
-                try oneOf(&ctx, "Hello, World"),
+                try parseAlt(&ctx, "Hello, World"),
             );
 
             try checkAndConsume(
                 ctx,
                 .initOk(.initSlice(.FOO, "Foo"), "Bar"),
-                try oneOf(&ctx, "FooBar"),
+                try parseAlt(&ctx, "FooBar"),
             );
 
             try checkAndConsume(
                 ctx,
                 .initFail("Hell or bust"),
-                try oneOf(&ctx, "Hell or bust"),
+                try parseAlt(&ctx, "Hell or bust"),
+            );
+        }
+
+        pub fn left(lp: Parser, rp: Parser) Parser {
+            const shim = struct {
+                fn leftParser(ctx: *Context, input: []const u8) ZpcError!Result {
+                    const lres = try lp(ctx, input);
+                    errdefer lres.deinit(ctx.allocator);
+                    if (!lres.matched()) return .initFail(input);
+                    const rres = try rp(ctx, lres.rest);
+                    defer rres.deinit(ctx.allocator);
+                    if (!rres.matched()) return .initFail(input);
+                    return .initOk(lres.tok.ok, rres.rest);
+                }
+            };
+            return shim.leftParser;
+        }
+
+        test left {
+            const parseLeft = left(
+                lit(.FOO, "Foo"),
+                lit(.BAR, "Bar"),
+            );
+
+            var ctx: TestContext = .{ .allocator = std.testing.allocator };
+
+            try checkAndConsume(
+                ctx,
+                .initOk(.initSlice(.FOO, "Foo"), "Baz"),
+                try parseLeft(&ctx, "FooBarBaz"),
+            );
+
+            try checkAndConsume(
+                ctx,
+                .initFail("FooBaz"),
+                try parseLeft(&ctx, "FooBaz"),
+            );
+
+            try checkAndConsume(
+                ctx,
+                .initFail("BarFoo"),
+                try parseLeft(&ctx, "BarFoo"),
+            );
+        }
+
+        pub fn right(lp: Parser, rp: Parser) Parser {
+            const shim = struct {
+                fn rightParser(ctx: *Context, input: []const u8) ZpcError!Result {
+                    const lres = try lp(ctx, input);
+                    defer lres.deinit(ctx.allocator);
+                    if (!lres.matched()) return .initFail(input);
+                    const rres = try rp(ctx, lres.rest);
+                    if (!rres.matched()) return .initFail(input);
+                    return rres;
+                }
+            };
+            return shim.rightParser;
+        }
+
+        test right {
+            const parseRight = right(
+                lit(.FOO, "Foo"),
+                lit(.BAR, "Bar"),
+            );
+
+            var ctx: TestContext = .{ .allocator = std.testing.allocator };
+
+            try checkAndConsume(
+                ctx,
+                .initOk(.initSlice(.BAR, "Bar"), "Baz"),
+                try parseRight(&ctx, "FooBarBaz"),
+            );
+
+            try checkAndConsume(
+                ctx,
+                .initFail("FooBaz"),
+                try parseRight(&ctx, "FooBaz"),
+            );
+
+            try checkAndConsume(
+                ctx,
+                .initFail("BarFoo"),
+                try parseRight(&ctx, "BarFoo"),
             );
         }
     };
